fbb602232fdbf276236b999cc3ac380794e94a30
Raiymbek <40425204+MrAtkens@users.noreply.github.com>
2021-11-04 10:04:37 +0600

    first stage complete

--- Assets/Scripts/CardLogic.cs
+++ Assets/Scripts/CardLogic.cs
@@ -1,18 +1,77 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 
-public class CardLogic : MonoBehaviour
+public class CardLogic : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler
 {
-    // Start is called before the first frame update
-    void Start()
+    //Игровая камера, отступ, класс перемещения карты и карты буфера, также сама карта буфер
+    Camera MainCamera;
+    Vector3 offset;
+    //Поля на которых находятся карты
+    public Transform DefaultParent, DefaultBufferCard;
+    GameObject BufferCard;
+
+    void Awake()
     {
-        
+        //Инициализация карты буфера и нашей единственной камеры
+        MainCamera = Camera.allCameras[0];
+        BufferCard = GameObject.Find("BufferCard");
     }
 
-    // Update is called once per frame
-    void Update()
+    public void OnBeginDrag(PointerEventData eventData)
     {
-        
+        // Выставляем отступ во время перетягивание и присваеваем перетягивание к нашим картам
+        offset = transform.position - MainCamera.ScreenToWorldPoint(eventData.position);
+        DefaultParent = DefaultBufferCard = transform.parent;
+
+        // Выставляем местоположение карты на игровом поле
+        BufferCard.transform.SetParent(DefaultParent);
+        BufferCard.transform.SetSiblingIndex(transform.GetSiblingIndex());
+
+        transform.SetParent(DefaultParent.parent);
+        //Нужно для возможность включение взаймодействия с картой на canvas который стал рукой игрока 
+        GetComponent<CanvasGroup>().blocksRaycasts = false;
+    }
+
+    public void OnDrag(PointerEventData eventData)
+    {
+        //Перемещение карты по столу
+        Vector3 newPosition = MainCamera.ScreenToWorldPoint(eventData.position);
+        transform.position = newPosition + offset;
+
+        if (BufferCard.transform.parent != DefaultBufferCard)
+            BufferCard.transform.SetParent(DefaultBufferCard);
+
+        //Определение позиций карты относительно других карт
+        CheckPoisition();
+    }
+
+    public void OnEndDrag(PointerEventData eventData)
+    {
+        transform.SetParent(DefaultParent);
+        GetComponent<CanvasGroup>().blocksRaycasts = true;
+
+        //Взаймодействие с временной картой
+        transform.SetSiblingIndex(BufferCard.transform.GetSiblingIndex());
+        BufferCard.transform.SetParent(GameObject.Find("Canvas").transform);
+        BufferCard.transform.localPosition = new Vector3(2538, 200); 
+    }
+
+
+    void CheckPoisition()
+    {
+        int newIndex = DefaultBufferCard.childCount;
+        // Здесь мы исходим из логики того что если карта которую мы хотим переместить по индексу меньше то она должна быть слева
+        for(int i = 0; i < DefaultBufferCard.childCount; i++)
+        {
+            if (transform.position.x < DefaultBufferCard.GetChild(i).position.x)
+            {
+                newIndex = i;
+                if (BufferCard.transform.GetSiblingIndex() < newIndex)
+                    newIndex--;
+                break;
+            }
+        }
+        //Устанавливаем позицию карты
+        BufferCard.transform.SetSiblingIndex(newIndex);
     }
 }
---
+++ Assets/Scripts/FieldLogic.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+
+namespace Assets.Scripts
+{
+    public class FieldLogic : MonoBehaviour, IDropHandler, IPointerEnterHandler, IPointerExitHandler
+    {
+        public void OnDrop(PointerEventData eventData)
+        {
+            CardLogic card = eventData.pointerDrag.GetComponent<CardLogic>();
+            //Выставление карты на стол если данный объект является картой
+            if (card)
+                card.DefaultParent = transform;
+
+        }
+
+        public void OnPointerEnter(PointerEventData eventData)
+        {
+            //Проверка на перетягивание карты
+            if (eventData.pointerDrag == null)
+                return;
+            //данное поле становится родителем для карты
+            CardLogic card = eventData.pointerDrag.GetComponent<CardLogic>();
+            if (card)
+                card.DefaultBufferCard = transform;
+        }
+
+        public void OnPointerExit(PointerEventData eventData)
+        {
+            //Проверка на перетягивание карты
+            if (eventData == null)
+                return;
+
+            CardLogic card = eventData.pointerDrag.GetComponent<CardLogic>();
+            if (card && card.DefaultBufferCard == transform)
+                card.DefaultBufferCard = card.DefaultParent;
+        }
+    }
+}
