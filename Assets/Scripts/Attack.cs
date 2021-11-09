651e15aed083fb1d8ebe75f818a85c3d4c96a8f7
Raiymbek <40425204+MrAtkens@users.noreply.github.com>
2021-11-10 02:07:20 +0600

    3 part completed

---
+++ Assets/Scripts/Attack.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class Attack : MonoBehaviour, IDropHandler
+{
+    public void OnDrop(PointerEventData eventData)
+    {
+        CardInfo card = eventData.pointerDrag.GetComponent<CardInfo>();
+
+        // Если данная карта может атаковать 
+        if (card && card.SelfCard.CanAttack &&
+            transform.parent == GetComponent<CardMovement>().GameManager.EnemyField)
+        {
+            // Переводим состояние атаки на false
+            card.SelfCard.ChangeAttackState(false);
+            
+            if(card.IsPlayer)
+                card.HighliteOff();
+            // Бой карт между друг другом
+            GetComponent<CardMovement>().GameManager.CardsFight(card, GetComponent<CardInfo>());
+        }
+    }
+}
