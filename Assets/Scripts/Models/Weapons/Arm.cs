7eb1fe705a49f240192e9144fa8888102ec27102
Raiymbek <40425204+MrAtkens@users.noreply.github.com>
2021-11-16 19:33:56 +0600

    Strategy pattern, random Weapon system

---
+++ Assets/Scripts/Models/Weapons/Arm.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+namespace Models.Weapons
+{
+    public class Arm : AbstractWeapon
+    {
+        public Arm() : base(1, 0, WeaponIcons.ArmIcon)
+        {
+        }
+
+        public override int GetDurability()
+        {
+            return Durability;
+        }
+
+        public override int UseWeapon()
+        {
+            Durability = Mathf.Clamp(Durability - 1, 0, int.MaxValue); 
+            Debug.Log("Weapon damage: " + GetType() + " Durability " + Durability + " Damage " + Damage);
+            return 0;
+        }
+    }
+}
---
+++ Assets/Scripts/Models/Weapons/WeaponIcons.cs
@@ -0,0 +1,10 @@
+﻿namespace Models.Weapons
+{
+    public static class WeaponIcons
+    {
+        public static string ArmIcon { get; } = "Sprites/Cards/arm";
+        public static string SwordIcon { get; } = "Sprites/Cards/sword";
+        public static string AxeIcon { get; } = "Sprites/Cards/axe";
+        public static string MaceIcon { get; } = "Sprites/Cards/mace";
+    }
+}
